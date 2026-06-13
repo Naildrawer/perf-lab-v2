bc58bdfc2ab54650712257243be64ae77f1ab529
fsocmr <sorokin.maksim.ev@gmail.com>
2026-06-13 21:02:14 +0000

    Add Nginx LB and multi instance Grafana monitoring

--- app/src/main/java/com/naildrawer/app/controller/GreetingController.java
+++ app/src/main/java/com/naildrawer/app/controller/GreetingController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
+    @GetMapping("/instance")
+public String instance() {
+    return System.getenv("HOSTNAME");
+}
+
     @GetMapping("/hello")
     public String hello() {
         return "Hello Performance Engineer!";
